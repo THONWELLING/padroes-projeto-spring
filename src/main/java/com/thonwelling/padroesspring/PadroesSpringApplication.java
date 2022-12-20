6edf1d2f675df7f213240bf1682ae1a4bce9129c
THONWELLING <thonwellingdani13@gmail.com>
2022-12-20 00:02:05 -0300

    Project Created

---
+++ src/main/java/com/thonwelling/padroesspring/PadroesSpringApplication.java
@@ -0,0 +1,14 @@
+package com.thonwelling.padroesspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PadroesSpringApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(PadroesSpringApplication.class, args);
+	}
+
+}
